3e147054626f0d26f6d88ceed3b1ad94a6c7f055
Helal Uddin Ovi <mdhelalud01@gmail.com>
2026-08-24 08:11:53 +0600

    Time: 0 ms (100%), Space: 8.3 MB (86.26%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+public:
+    string build(string s)
+    {
+        string ans;
+
+        for (char ch : s)
+        {
+            if (ch == '#')
+            {
+                if (!ans.empty())
+                {
+                    ans.pop_back();
+                }
+            }
+            else
+            {
+                ans.push_back(ch);
+            }
+        }
+
+        return ans;
+    }
+
+    bool backspaceCompare(string s, string t)
+    {
+        return build(s) == build(t);
+    }
+};
